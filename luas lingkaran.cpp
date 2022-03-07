fe80604a70b394229b43cb57119819b2979f3d95
Peru Pagustian <98791516+PeruPagustian@users.noreply.github.com>
2022-03-08 00:32:52 +0700

    Initial commit

---
+++ luas lingkaran.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	const float phi = 3.14;
+	float L, r;
+	
+	//value r
+	cout<<"masukkan jari-jari lingkaran =";
+	cin>>r;
+	
+	//proses perhitungan luas
+	L = phi * r * r;
+	
+	//pemanis
+	cout<<"diketahui lingkaran dengan = "<<endl;
+	cout<<"phi \t\t ="<<phi<<endl;
+	cout<<"r \t\t = "<<r<<endl;
+	cout<<"====================="<<endl;
+	//penampilan hasil
+	cout<<"Luas lingkaran dengan jari-jari "<<r<<" cm "<<" adalah ";
+	cout<<L<<" cm2";
+}
